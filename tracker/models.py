f9b530c8aebb1a51cba15aa610b284996ecac8db
Akeem Joko <g00366442@atu.ie>
2025-03-28 22:42:55 +0000

    Visualizations added

--- tracker/models.py
+++ tracker/models.py
@@ -85,3 +85,14 @@ class APIKey(models.Model):
 
     def __str__(self):
         return f"API Keys for {self.user.username}"
+
+class PortfolioHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    total_value = models.DecimalField(max_digits=20, decimal_places=2)
+    
+    class Meta:
+        ordering = ['-timestamp']
+        indexes = [
+            models.Index(fields=['user', 'timestamp'])
+        ]
