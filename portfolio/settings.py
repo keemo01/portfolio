78569c32ac2ec2d2b03b30caa92d8e6d7c7ae3b2
Akeem Joko <g00366442@atu.ie>
2025-04-25 15:52:50 +0100

    Prepare for Heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,7 +35,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1*iy9qf5otbk9t@w8fy@c&_$5sl$ean46*feqb4nf%u6zd-5y3"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
